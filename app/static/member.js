671732da46e597520ff55296650934458685c8a0
Stephen Fox <stephenfox995@hotmail.com>
2016-04-13 22:57:29 +0100

    Members can now be updated

--- app/static/member.js
+++ app/static/member.js
@@ -16,6 +16,18 @@ function Member(member_id, member_f_name, member_l_name, member_handicap) {
   this.get_member_handicap = function() {
     return this.member_handicap;
   }
+  this.set_member_id = function(member_id) {
+    this.member_id = member_id;
+  }
+  this.set_member_f_name = function(member_f_name) {
+    this.member_f_name = member_f_name;
+  }
+  this.set_member_l_name = function(member_l_name) {
+    this.member_l_name = member_l_name;
+  }
+  this.set_member_handicap = function(member_handicap) {
+    this.member_handicap = member_handicap;
+  }
   this.jsonify = function() {
     var jsonData = {
       "firstname" :this.get_member_f_name(),
