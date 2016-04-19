1995c9e433d0c96e1e32dc3f7f1f7b0f2c992cd0
Stephen Fox <stephenfox995@hotmail.com>
2016-04-19 22:09:19 +0100

    Added bootstrap

---
+++ app/static/styles/common.js
@@ -0,0 +1,15 @@
+function ajax_get_member(member_id) {
+
+}
+
+function ajax_get_event(event_id) {
+
+}
+
+function ajax_get_result_for_event(event_id) {
+
+}
+
+function ajax_get_result_for_member(member_id) {
+
+}
