4217455dcc165d864f2fad385c1cbc2cfa48a663
Stephen Fox <stephenfox995@hotmail.com>
2016-04-20 22:11:08 +0100

    Modularised POST method add_member into common.js file

--- app/static/common.js
+++ app/static/common.js
@@ -1,3 +1,36 @@
+/**
+Attempts to add a member to the database.
+@param member The member object to add.
+@param success A callback upon a successful insertion
+               into the database.
+@param failer A callback when an error occurs.
+*/
+function ajaxPOST_add_member(member, successful, failure) {
+  var member_f_name = member.get_member_f_name();
+  var member_l_name = member.get_member_l_name();
+  var member_handicap = member.get_member_handicap();
+
+  var jsonData = {
+    'firstname': member_f_name,
+    'lastname' : member_l_name,
+    'handicap' : member_handicap
+  }
+  var data = JSON.stringify(jsonData);
+
+  $.ajax({
+    type: 'POST',
+    url: '/add_member/',
+    contentType: 'application/json',
+    data: data,
+    success: function(data) {
+      successful(data);
+    },
+    failure: function() {
+      failure();
+    }
+  });
+}
+
 function ajax_get_member(member_id) {
 
 }
