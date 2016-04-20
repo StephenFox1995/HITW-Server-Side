77860dbb674922e6316e58ef3f58f25252a2a3f7
Stephen Fox <stephenfox995@hotmail.com>
2016-04-20 22:27:33 +0100

    Modularised POST method add_event into common.js file

--- app/static/common.js
+++ app/static/common.js
@@ -1,9 +1,9 @@
 /**
 Attempts to add a member to the database.
 @param member The member object to add.
-@param success A callback upon a successful insertion
+@param successful A callback upon a successful insertion
                into the database.
-@param failer A callback when an error occurs.
+@param failure A callback when an error occurs.
 */
 function ajaxPOST_add_member(member, successful, failure) {
   var member_f_name = member.get_member_f_name();
@@ -31,6 +31,41 @@ function ajaxPOST_add_member(member, successful, failure) {
   });
 }
 
+
+/**
+Attempts to add a event to the database.
+@param event The event to add.
+@param successful A callback upon a successful
+                  insertion into the database.
+@param failure A callback when an error occurs.
+*/
+function ajaxPOST_add_event(event, successful, failure) {
+  var event_title = event.get_event_title();
+  var event_location = event.get_event_location();
+  var event_time = event.get_event_time();
+  var event_date = event.get_event_date();
+
+  var jsonData = {
+    'title'     :event_title,
+    'location'  :event_location,
+    'time'      :event_time,
+    'date'      :event_date
+  };
+  var data = JSON.stringify(jsonData);
+
+  $.ajax({
+    type: 'POST',
+    url: '/add_event/',
+    contentType: 'application/json',
+    data: data,
+    success: function(data) {
+      successful();
+    },
+    failure: function() {
+      failure();
+    }
+  });
+}
 function ajax_get_member(member_id) {
 
 }
