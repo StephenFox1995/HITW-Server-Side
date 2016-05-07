80c85bf69f491df2a494dd6c9211d6b34f58e052
Stephen Fox <stephenfox995@hotmail.com>
2016-05-07 15:13:34 +0100

    Added heading to results page

--- app/static/gallery.js
+++ app/static/gallery.js
@@ -1,6 +1,6 @@
 function addGallery(element, images) {
   if (images instanceof Array) {
-    $(".container").append(galleryBody());
+    $(element).append(galleryBody());
 
     images.forEach(function(image) {
       $('.row').append(galleryItem(image));
@@ -13,6 +13,7 @@ function galleryItem(url) {
   return '<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350"></a></div>';
 }
 
+// Reuturns the css for a gallery item.
 function galleryBody() {
   var body =
       '<h1>Gallery</h1>' +
