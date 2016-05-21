39bebdaec17d30eab18b868a5390d3f8f857bd67
Stephen Fox <stephenfox995@hotmail.com>
2016-05-21 18:42:21 +0100

    Fixed bug where clicking on an event would use the td index instead of the tr.

--- app/static/gallery.js
+++ app/static/gallery.js
@@ -3,23 +3,26 @@ function addGallery(element, images) {
     $(element).append(galleryBody());
 
     images.forEach(function(image) {
-      $('.row').append(galleryItem(image));
+      $('.gallery').append('row', galleryItem(image));
     });
   }
+
+  $('.gallery').bootstrapGallery();
+  $('.gallery-overview').bootstrapGallery({
+    iconset: "fontawesome"
+  });
+  $('.gallery').justifyGallery();
 }
 
 // Returns the ccs for a gallery item.
 function galleryItem(src) {
-  return '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">' +
-            '<img class="img-responsive" src="' + src + '">' +
-         '</li>';
+  return '<a class="col-xs-6 col-sm-4" href="' + src + '">' +
+              '<img src= "' + src + '">' +
+          '</a>';
 }
 
 // Reuturns the css for a gallery item.
 function galleryBody() {
-  var body =
-      '<h1>Gallery</h1>' +
-      '<ul class="row" style="width: inherit;"></ul>'
-
-  return body;
+  var body = '<h1>Gallery</h1>';
+    return body;
 }
